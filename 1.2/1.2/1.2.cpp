9fcc2333b470c5ad0a9578a274d476d09d87f62d
myownrhyme <lxyownrhyme@gmail.com>
2015-03-06 15:23:28 +0800

    1.1-1.3_c++_primer

---
+++ 1.2/1.2/1.2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main(){
+
+	std::cout << "hello world !" << std::endl;//1.3
+	int a = 2;
+	int b = 3;
+	std::cout << 2 * 3 << std::endl;//1.4
+	std::cout << "The sum of " << a//1.5-1.6
+			  << "and" << b
+			  << "is" << a + b << std::endl;
+}
